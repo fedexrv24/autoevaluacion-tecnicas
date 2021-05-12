0bf08b3f3b6ba2ccb839c629a53c6e82e25d72cb
Federico <federico.fidelibus@alumnos.ispi4038.edu.ar>
2021-05-12 20:46:08 -0300

    SP.Reviso si es lo que necesito, lo guardo y solicito otro numero.

--- 002.py
+++ 002.py
@@ -1,3 +1,16 @@
 prom = 0
 tot = 0
 numeros = input("Ingrese numero: ").strip()
+
+while True:
+        if len(numeros) == 0:
+                break
+        try:
+                decimal = float(numeros)
+                tot += decimal
+                prom += 1
+
+        except ValueError:
+                print("Error. Ingrese un numero.")
+
+        numeros = input("Ingrese numero o enter para salir: ").strip()
